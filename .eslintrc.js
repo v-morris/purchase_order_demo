8011311d617f06ffbeb9cfda6271c2ca785ad5e7
V Morris <digidarkdev@gmail.com>
2023-04-29 16:13:01 -0700

    feat(purchaseorderdemo): initialize project with ESLint, CommitLint, Commitizen, Husky

---
+++ .eslintrc.js
@@ -0,0 +1,26 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+    jest: true,
+  },
+  extends: [
+    'plugin:react/recommended',
+    'airbnb',
+  ],
+  overrides: [
+  ],
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+  },
+  plugins: [
+    'react',
+  ],
+  rules: {
+    'linebreak-style': 0,
+    'no-tabs': 0,
+    'react/jsx-props-no-spreading': 0,
+    'no-unused-vars': 1,
+  },
+};
